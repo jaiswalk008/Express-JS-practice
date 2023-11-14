aa680cad8f122ca091359e66c08c0eae5d1bde83
jaiswalk008 <jaiskaran008@gmail.com>
2023-11-14 22:01:02 +0530

    fixed some issues

---
+++ routes/admin.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+const rootDir = require('../helper/path');
+router.get('/add-product',(req,res,next)=>{
+    res.sendFile(path.join(rootDir,'views','add-product.html'))
+});
+router.post('/admin/add-product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+})
+module.exports = router;
